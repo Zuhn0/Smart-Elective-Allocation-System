6656a97b43d42abf64db3039bb2d5545257d4cef
Zuhn0 <harshsingh0063@gmail.com>
2026-08-02 00:15:31 +0530

    Downgrade Prisma to v6

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,12 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({ adapter });
-
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+const prisma = new PrismaClient();
 
 module.exports = prisma;
